8dbfdb2955bbc3bdde2308fc0c295dc7b182ee59
Kaiprivate99 <68280228+Kaiprivate99@users.noreply.github.com>
2022-11-21 22:53:55 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小米虫',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '9986e12cd3e77cdd41ca7bfec6c8f138',
+      id: 'oCnhz6BZMJPcKLQpoa-Sm3Scc-pQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
+      useTemplateId: 'zSMthO64duxWsn7ZotCaqjK0lTlWJb1qQgPejZS8vbM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-20',
       festivals: [
